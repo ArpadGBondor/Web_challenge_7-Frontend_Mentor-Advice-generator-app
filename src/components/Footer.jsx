5693de2660cce87f3a038660f16ff0da39633693
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2024-07-03 03:16:31 +0100

    fix: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
 
 function Footer() {
-    return (
-        <footer>
-            <div className="attribution">
-                <p>
-                    Challenge by{' '}
-                    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
-                        Frontend Mentor
-                    </a>
-                    .
-                </p>
-                <p>
-                    Coded by{' '}
-                    <a href="https://arpadgbondor.github.io/CV/" target="_blank" rel="noreferrer">
-                        Árpád Gábor Bondor
-                    </a>{' '}
-                    in 2022.
-                </p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className="attribution">
+        <p>
+          Challenge from{' '}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          .
+        </p>
+        <p>
+          Completed by{' '}
+          <a
+            href="https://arpadgbondor.github.io/CV/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Árpád Gábor Bondor
+          </a>{' '}
+          in 2022.
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
